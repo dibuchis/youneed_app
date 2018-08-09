27ac0b802fe25ceb9a41cb58444ecc8ee81543ef
Juan Cedillo <info@abitmedia.com>
2018-08-09 14:06:15 -0500

    Se integra conexion con api de traccar

--- controllers/ApiController.php
+++ controllers/ApiController.php
@@ -59,6 +59,36 @@ class ApiController extends Controller
       return true;  
     }
 
+    private function setHeader($status)
+    {
+ 
+      $status_header = 'HTTP/1.1 ' . $status . ' ' . $this->_getStatusCodeMessage($status);
+      $content_type="application/json; charset=utf-8";
+ 
+      header($status_header);
+      header('Content-type: ' . $content_type);
+      header('X-Powered-By: ' . "Abitmedia <abitmedia.com>");
+      header("Access-Control-Allow-Origin: *");
+    }
+
+    private function _getStatusCodeMessage($status)
+    {
+      // these could be stored in a .ini file and loaded
+      // via parse_ini_file()... however, this will suffice
+      // for an example
+      $codes = Array(
+          200 => 'OK',
+          400 => 'Bad Request',
+          401 => 'Unauthorized',
+          402 => 'Payment Required',
+          403 => 'Forbidden',
+          404 => 'Not Found',
+          500 => 'Internal Server Error',
+          501 => 'Not Implemented',
+      );
+      return (isset($codes[$status])) ? $codes[$status] : '';
+    }
+
     public function actionGetinfoapp( $token = null ){
       if( is_null( $token ) ){
         $this->setHeader(200);
@@ -204,32 +234,4 @@ class ApiController extends Controller
       }
     }
 
-    private function setHeader($status)
-      {
- 
-      $status_header = 'HTTP/1.1 ' . $status . ' ' . $this->_getStatusCodeMessage($status);
-      $content_type="application/json; charset=utf-8";
- 
-      header($status_header);
-      header('Content-type: ' . $content_type);
-      header('X-Powered-By: ' . "Abitmedia <abitmedia.com>");
-      header("Access-Control-Allow-Origin: *");
-      }
-    private function _getStatusCodeMessage($status)
-    {
-    // these could be stored in a .ini file and loaded
-    // via parse_ini_file()... however, this will suffice
-    // for an example
-    $codes = Array(
-        200 => 'OK',
-        400 => 'Bad Request',
-        401 => 'Unauthorized',
-        402 => 'Payment Required',
-        403 => 'Forbidden',
-        404 => 'Not Found',
-        500 => 'Internal Server Error',
-        501 => 'Not Implemented',
-    );
-    return (isset($codes[$status])) ? $codes[$status] : '';
-    }
 }
