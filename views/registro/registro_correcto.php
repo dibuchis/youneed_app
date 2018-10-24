a9155f974f3da2be41574ffcd8b328e2f05dd96c
Juan Cedillo <info@abitmedia.com>
2018-10-24 13:52:02 -0500

    Se aumenta imagenes para registro exitoso e incorrecto

--- views/registro/registro_correcto.php
+++ views/registro/registro_correcto.php
@@ -1,6 +1,13 @@
+<?php 
+use yii\helpers\Url;
+use yii\helpers\Html;
+?>
 <div class="container">
 	<p>&nbsp;</p>
 	<div class="alert alert-success">
 	  <strong>Registro exitoso!</strong> Gracias por su información, nos pondremos en contacto con usted en los próximos días.
 	</div>
+	<center>
+		<?php echo Html::img( Url::to('@web/images/registro.jpg'), ['class'=>'img-responsive'] ); ?>
+	</center>
 </div>
--- views/registro/registro_incorrecto.php
+++ views/registro/registro_incorrecto.php
@@ -1,9 +1,16 @@
+<?php 
+use yii\helpers\Url;
+use yii\helpers\Html;
+?>
 <div class="container">
 	<p>&nbsp;</p>
 	<div class="alert alert-danger">
 	  <strong>Error!</strong> No se pudo realizar el registro, vuelva a intentarlo.
 	</div>
 </div>
+<center>
+	<?php echo Html::img( Url::to('@web/images/error.jpg'), ['class'=>'img-responsive'] ); ?>
+</center>
 <pre>
 	<?php print_r($model->getErrors()); ?>
 </pre>
