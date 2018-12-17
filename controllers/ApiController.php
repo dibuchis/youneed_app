a1804a49b6f50a65f6ac98863fff9219a70f031c
Juan Carlos <info@abitmedia.com>
2018-12-16 21:53:49 -0500

    se integra busqueda en metodo getsrvices

--- controllers/ApiController.php
+++ controllers/ApiController.php
@@ -413,7 +413,7 @@ class ApiController extends Controller
         $array_servicios[] = [
                                 'id' => $servicio->id,
                                 // 'nombre' => ( is_null( $servicio_id ) ) ? mb_convert_encoding( trim(substr( $servicio->nombre, 0, 100 )).'...' , 'UTF-8', 'UTF-8' ) : $servicio->nombre,
-                                'name' => $servicio->nombre,
+                                'nombre' => $servicio->nombre,
                                 'incluye' => ( is_null( $servicio_id ) ) ? mb_convert_encoding( trim( substr( strip_tags($servicio->incluye), 0, 80 ) ).'...', 'UTF-8', 'UTF-8' ) : $servicio->incluye,
                                 // 'imagen' => $servicio->imagen,
                                 'no_incluye' => $servicio->no_incluye,
