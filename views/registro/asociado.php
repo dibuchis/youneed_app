daddbd01ecdc772b5abca9c78623395d63cfc9f0
dibuchis <dibuchis@gmail.com>
2019-04-10 21:53:15 -0500

    Agregar Página de Beneficios de Asociado

--- views/registro/asociado.php
+++ views/registro/asociado.php
@@ -482,7 +482,7 @@ use borales\extensions\phoneInput\PhoneInput;
 
 				            <?= $form->field($model, 'plan_id')->hiddenInput()->label(false); ?>
 				        
-							<p>Antes de registrarse lea los <a href="/registro/terminos" target="_blank">Términos y condiciones</a> y <a href="/registro/acuerdos" target="_blank">Acuerdos de Asociado</a> para aceptar su plan.</p>
+							<p>Antes de registrarse lea los <a href="/registro/terminos" target="_blank">Términos y condiciones</a> y <a href="/registro/aceptacion" target="_blank">Acuerdos de Asociado</a> para aceptar su plan.</p>
 				        	<!-- <div style="height: 200px!important; overflow-y: scroll;" > -->
 								<!-- <?php //echo $terminos; ?> -->
 				        		<!-- </div> -->
