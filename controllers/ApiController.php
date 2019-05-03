fe19d92b4fb51d03ae347ac4058d0fb90554a638
dibuchis <dibuchis@gmail.com>
2019-05-03 16:41:30 -0500

    Módulo Notificaciones y Proceso Contrato de Servicios

--- controllers/ApiController.php
+++ controllers/ApiController.php
@@ -46,8 +46,24 @@ class ApiController extends Controller
               'setshoppingcart'=>['get'],
               'transmittotraccar'=>['get'],
           ],
+
    
-          ]
+        ],
+        'corsFilter' => [
+          'class' => \yii\filters\Cors::className(),
+          'cors' => [
+              // restrict access to
+              'Origin' => ['https://youneed.com.ec', 'https://www.youneed.com.ec'],
+              // Allow only POST and PUT methods
+              'Access-Control-Request-Method' => ['POST', 'PUT'],
+              // Allow only headers 'X-Wsse'
+              'Access-Control-Request-Headers' => ['X-Wsse'],
+              // Allow credentials (cookies, authorization headers, etc.) to be exposed to the browser
+              'Access-Control-Allow-Credentials' => true,
+              // Allow OPTIONS caching
+              'Access-Control-Max-Age' => 3600,
+              // Allow the X-Pagination-Current-Page header to be exposed to the browser.
+              'Access-Control-Expose-Headers' => ['X-Pagination-Current-Page'],
       ];
     }
  
